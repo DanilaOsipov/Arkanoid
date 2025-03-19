cfa147ef1059e61108423b9fc2084caf9e19a754
Danila Osipov <d.osssipov@gmail.com>
2025-03-19 22:32:17 +0400

    Implement fail logic

---
+++ Arkanoid/Assets/Scripts/Gameplay/BallMissingZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gameplay
+{
+    public class BallMissingZone : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onBallMissing = new UnityEvent();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponentInParent<Ball>() != null)
+                _onBallMissing.Invoke();
+        }
+    }
+}
