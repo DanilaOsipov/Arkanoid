cd51de8814ebb1e8de30f3de67dbf754763d140f
Danila Osipov <d.osssipov@gmail.com>
2025-03-19 20:52:48 +0400

    Import TMP. Implement bricks logic. Add win screen

---
+++ Arkanoid/Assets/Scripts/Gameplay/Brick.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Brick : MonoBehaviour
+    {
+        public event Action<Brick> OnBallCollisionEnter = delegate { };
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (!collision.gameObject.TryGetComponent<Ball>(out var ball))
+                return;
+
+            OnBallCollisionEnter(this);
+        }
+    }
+}
---
+++ Arkanoid/Assets/Scripts/Gameplay/BricksFacade.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gameplay
+{
+    public class BricksFacade : MonoBehaviour
+    {
+        [SerializeField] private Brick[] _bricks;
+        [SerializeField] private UnityEvent _onAllBricksDestroyed = new UnityEvent();
+
+        private void Awake()
+        {
+            foreach (var brick in _bricks)
+            {
+                brick.OnBallCollisionEnter += OnBallCollisionEnterHandler;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var brick in _bricks)
+            {
+                brick.OnBallCollisionEnter -= OnBallCollisionEnterHandler;
+            }
+        }
+
+        private void OnBallCollisionEnterHandler(Brick brick)
+        {
+            brick.gameObject.SetActive(false);
+            bool allDestroyed = _bricks.All(e => !e.gameObject.activeSelf);
+            if (allDestroyed) _onAllBricksDestroyed.Invoke();
+        }
+    }
+}
