be172ea50345c756359ed8f4d522960513df9ae0
Danila Osipov <d.osssipov@gmail.com>
2025-03-20 02:28:03 +0400

    Clamp platform position

---
+++ Arkanoid/Assets/Scripts/Gameplay/PositionClamper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class PositionClamper : MonoBehaviour
+    {
+        [SerializeField] private Transform _minPosition;
+        [SerializeField] private Transform _maxPosition;
+
+        private void Update()
+        {
+            transform.position = new Vector3
+            {
+                x = Mathf.Clamp(transform.position.x, _minPosition.position.x, _maxPosition.position.x),
+                y = Mathf.Clamp(transform.position.y, _minPosition.position.y, _maxPosition.position.y),
+                z = Mathf.Clamp(transform.position.z, _minPosition.position.z, _maxPosition.position.z)
+            };
+        }
+    }
+}
